0d4c907cb662ec5eb169bf8cd6663fe06bf83210
Neoflies <loinguyen@techkids.io>
2020-08-31 12:15:38 +0700

    Remove old file

--- src/modules/auth/aggregates/roles/interfaces/Role.ts
+++
@@ -1,9 +0,0 @@
-import { IsAuditable } from '@app/core';
-
-export interface Role extends IsAuditable {
-  _id: string;
-  name: string;
-  description: string;
-  permissions: string[];
-  isActive: boolean;
-}
